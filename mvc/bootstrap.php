160822092023b9f31f4a9a741cb0949cf9c5e448
Rafael Iuri Coppola <rafael.coppola@bmstecnologia.com.br>
2022-01-20 19:48:19 -0300

    iniciando modulo mvc

---
+++ mvc/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+// $controller = new SON\Controller;
+$controller1 = new App\Controllers\UsersController;
+
+// echo $controller->handler();
+echo $controller1->handler();
---
+++ mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c23c89cf613066add4a61a562ff06ed0219a8402',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c23c89cf613066add4a61a562ff06ed0219a8402',
+            'dev_requirement' => false,
+        ),
+    ),
+);
